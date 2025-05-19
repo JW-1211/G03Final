1ce89b43094834aee30b022273dac4cfafea2634
Kim jina <3636wlsdk@gmail.com>
2025-05-19 16:50:22 +0900

    Update I. Introduction.py

--- pages/I. Introduction.py
+++ pages/I. Introduction.py
@@ -1,11 +1,7 @@
 import streamlit as st
-
 st.title("I. Introduction")
-
 tab1, tab2 = st.tabs(["A four-panel summary of the story" , "..."])
-
 with tab1:
     st.write("First")
-
 with tab2:
     st.write("Second")
