982e41f0f64a2aa5ebd9713df4693c48fc7396ad
thelighterside <albertpark26@gmail.com>
2025-05-19 13:11:24 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🌟 Group III")
+st.caption("This is Group 3's super special secret project! Stay tuned for further updates.")
+
+st.write("This will be a collaboratory work...eventually.")
+st.markdown("---")
