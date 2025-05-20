146253eddc72e1f9fc6e400a93842a7c7d69c069
Kim jina <3636wlsdk@gmail.com>
2025-05-21 00:04:29 +0900

    Update I. Introduction.py

--- pages/I. Introduction.py
+++ pages/I. Introduction.py
@@ -8,7 +8,7 @@ tab1, tab2 = st.tabs(["A four-panel summary of the story", "..."])
 with tab1:
     st.subheader("Story Summary")
     image = Image.open("images/Compass.png")
-    st.image(image, caption="A four-panel comic", use_column_width=True)
+    st.image(image, caption="A four-panel comic", use_container_width=True)
 
 with tab2:
     st.write("Second tab content here.")
