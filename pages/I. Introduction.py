2380e55284270af61c9aece64f03976561e44ca3
Kim jina <3636wlsdk@gmail.com>
2025-05-20 11:19:40 +0900

    Update I. Introduction.py

--- pages/I. Introduction.py
+++ pages/I. Introduction.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from PIL import Image
 
 st.title("I. Introduction")
-tab1, tab2 = st.tabs(["A four-panel summary of the story" , "..."])
+
+tab1, tab2 = st.tabs(["A four-panel summary of the story", "..."])
 
 with tab1:
-    st.write("First")
+    st.subheader("Story Summary")
+    image = Image.open("images/Compass.png")
+    st.image(image, caption="A four-panel comic", use_column_width=True)
+
 with tab2:
-    st.write("Second")
+    st.write("Second tab content here.")
