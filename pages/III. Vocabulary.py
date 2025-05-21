2b7a66b0071bc201f6389752b295be6491feda62
JW <leejuwon9282@gmail.com>
2025-05-21 12:44:45 +0900

    Update III. Vocabulary.py

--- pages/III. Vocabulary.py
+++ pages/III. Vocabulary.py
@@ -15,7 +15,7 @@ with tab1:
   st.markdown("### 📋 Word Frequency Table")
 
    # Load CSV from GitHub (update the link below)
-  url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/data/word_frequency.csv"
+  url = "https://raw.githubusercontent.com/JW-1211/G03Final/main/data/vocabulary.csv"
   df = pd.read_csv(url)
 
     # Show table only when button is clicked
@@ -31,7 +31,7 @@ with tab2:
   
   # --- Load CSV from GitHub ---
 
-  url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/data/word_frequency.csv"  # ← replace this!
+  url = "https://raw.githubusercontent.com/JW-1211/G03Final/main/data/vocabulary.csv"  # ← replace this!
   df = pd.read_csv(url)
   
   # --- Dropdown to select word ---
@@ -54,7 +54,7 @@ with tab3:
     st.caption("Click the button to hear a word. Then type it and press 'Check the answer'.")
 
     # Load CSV
-    url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/data/word_frequency.csv"  # Replace this!
+    url = "https://raw.githubusercontent.com/JW-1211/G03Final/main/data/vocabulary.csv"  # Replace this!
     df = pd.read_csv(url)
     word_list = df["Word"].dropna().tolist()
 
