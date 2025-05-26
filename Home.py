ae9c8de4ea220c25ca6690e0383a5bc8c9342d74
thelighterside <albertpark26@gmail.com>
2025-05-26 12:46:21 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,5 +5,5 @@ st.set_page_config(page_title="Welcome!")
 st.title("🌟Getting Comfortable with English Literature🌟")
 st.caption("A masterpiece in progress by 김류경, 김진아, 이주원, 조선미 and 박인겸")
 
-st.write("This page is dedicated to the brilliant & ingenious language learning project developed by the dilligent and wise classmates of Group 3. Just follow the instructions, one at a time to get familiar with reading and writing!")
+st.write("This page is dedicated to the brilliant & ingenious language learning project developed by the dilligent and wise classmates of Group 3. Just follow the instructions, step by step, to get familiar with reading and writing!")
 st.markdown("---")
