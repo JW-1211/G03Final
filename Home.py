100bd361ea737566c65d1aebe0ba155d0e2c3031
thelighterside <albertpark26@gmail.com>
2025-05-26 12:29:57 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🌟Everything there is to be done for studying English literature🌟")
-st.caption("[A beautiful work in progress by 김류경, 김진아, 이주원, 조선미 and 박인겸]")
+st.caption("A masterpiece in progress by 김류경, 김진아, 이주원, 조선미 and 박인겸")
 
 st.write("This page is dedicated to the brilliant & ingenious language learning project developed by the dilligent and wise classmates of Group 3.")
 st.markdown("---")
