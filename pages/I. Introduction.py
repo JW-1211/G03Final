08048d1c055cc89b68963b091dd0ea5d7026b440
Kim jina <3636wlsdk@gmail.com>
2025-05-26 13:38:06 +0900

    Update I. Introduction.py

--- pages/I. Introduction.py
+++ pages/I. Introduction.py
@@ -1,14 +1,25 @@
 import streamlit as st
 from PIL import Image
 
-st.title("I. Introduction")
+st.title("I. Overview")
 
-tab1, tab2 = st.tabs(["💫Guess the story", "💫Guidelines"])
+tab1, tab2 = st.tabs(["💫Guidelines", "💫Guess the story"])
 
 with tab1:
+    st.subheader("Guidelines")
+    st.write("""
+1.  Let’s try to guess the overall flow of the story based on the four-panel comic.
+
+2.  Let’s try to predict the key words that appear in the story using the word cloud.
+
+So, what might happen to the main character?
+""")
+
+
+with tab2:
     st.subheader("Guess the story")
     st.write("Let's guess the story!")
-
+    
     col1, col2 = st.columns(2)
 
     with col1:
@@ -18,13 +29,3 @@ with tab1:
     with col2:
         image2 = Image.open("images/IMG_1605.jpeg")
         st.image(image2, caption="Word cloud", use_container_width=True)
-
-with tab2:
-    st.subheader("Guidelines")
-    st.write("""
-Let’s try to guess the overall flow of the story based on the four-panel comic pictures.
-
-Let’s try to predict the key words that appear in the story using the word cloud.
-
-So, what might happen to the main character?
-""")
