71fde0381a8770e79d3b5123444161bacc069de6
thelighterside <albertpark26@gmail.com>
2025-05-26 12:29:07 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🌟 Group III")
-st.caption("This is Group 3's super special secret project! Stay tuned for further updates.")
+st.title("🌟Everything there is to be done for studying English literature🌟")
+st.caption("[A beautiful work in progress by 김류경, 김진아, 이주원, 조선미 and 박인겸]")
 
-st.write("This will be a collaboratory work...eventually.")
+st.write("This page is dedicated to the brilliant & ingenious language learning project developed by the dilligent and wise classmates of Group 3.")
 st.markdown("---")
