733177ea8551bcaf670bb72f586bec9b7a5db77f
thelighterside <albertpark26@gmail.com>
2025-06-04 06:56:34 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -8,14 +8,14 @@ Welcome to the **English Story Grammar and Vocabulary Analyzer**!
 This application helps readers, learners, and educators better understand the grammar and vocabulary used in English stories.
 """)
 
-st.header("This application consists of:")
+st.header("This app consists of five parts in total:")
 st.write("""
 
-- **Overview**
-- **Read with audio**
-- **Vocabulary**
-- **Grammar**
-- **Thinking beyond**
+- **1. Overview**: Warm-up activities including guesswork and sharing ideas
+- **2. Read with audio**: Practicing reading the story aloud with audio
+- **3. Vocabulary**: Memorizing key words and definitions, and expanding vocabulary with synonyms/antonyms
+- **4. Grammar**: Studying rules and examples involving verbs with regular & irregular forms of past tense
+- **5. Thinking beyond**: Creative thinking, creative writing and modernized sharing methods
 """)
 
 st.header("How It Works")
