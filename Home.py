e555dbf6b44179ee47fe9f5c24b159c810576728
thelighterside <albertpark26@gmail.com>
2025-06-04 11:18:07 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Welcome!")
 
-st.title("🌟Getting Comfortable with English Writing🌟")
+st.title("🌟Getting Comfortable with Written English🌟")
 st.caption("A masterpiece by 김류경, 김진아, 이주원, 조선미 and 박인겸")
 
 st.write("This page is dedicated to the brilliant & ingenious language learning project developed by the dilligent and wise classmates of Group 3. Just follow the instructions, step by step, to get familiar with reading and writing in English!")
